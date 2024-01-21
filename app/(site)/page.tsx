69890a3dfd5bec0143cca9e6ae939253010ff994
haikaljs <haikaljavascript@gmail.com>
2024-01-21 20:21:22 +0800

    Button, Header component

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,3 +1,14 @@
+import Header from "@/components/Header";
+
 export default function Home() {
-  return <div className="text-green-500">Main content</div>;
+  return (
+    <div
+      className="
+  bg-neutral-900 rounded-lg h-full w-full overflow-hidden overflow-y-auto"
+    >
+      <Header>
+        header
+      </Header>
+    </div>
+  );
 }
